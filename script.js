61703fedce3575e8638391977448cbe1d76b9434
alineantunesrocha-boop <aline.antunes.rocha@escola.pr.gov.br>
2026-04-06 10:45:36 -0300

    salvar

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
 //Corrige o ul da agenda caso a classe nao esteja certa
 const secaAgenda = document.querySelector("#agenda");
+ if(secaoAgenda) {
+    const ulAgenda = secaoAgenda.querySelector("ul");
+    if (ulAgenda) {
+        ulAgenda.classList.add("agenda")
+    }
+ }
 
-
+ const secaoContato = document.querySelector("#contato");
+  if(secaoContato){
+    secaoContato.classList.add(contato-box);
+  }
 });
