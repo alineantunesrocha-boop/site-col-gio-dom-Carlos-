10db68ff7ef5e7cfa2f48b6042a77a4119e849ee
alineantunesrocha-boop <aline.antunes.rocha@escola.pr.gov.br>
2026-04-06 11:09:19 -0300

    salvar

--- script.js
+++ script.js
@@ -16,4 +16,15 @@ const secaAgenda = document.querySelector("#agenda");
   //seleciona links do menu
   const linksMenu = document.querySelector("nav a");
   const secoes = document.querySelectorAll("section[id]");
+
+  //Deixa a transição suave nos cards via js
+  const cards = document.querySelectorAll(".card");
+  for(let i = 0; i < cards.length; i++){
+    cards[i].style.transition = "0.3s ease";
+
+    cards[i].addEventListener("mouseenter", function(){
+     this.style.transform = "translateY(-6px)";
+    });
+    cards[i].style.transform =
+  }
 });
